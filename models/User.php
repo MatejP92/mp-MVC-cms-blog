e01cec705f639697d58d7748b1ea50a742064616
Matej~ <matej.pal@hotmail.com>
2022-12-15 16:46:43 +0100

    innitial commit

---
+++ models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\models;
+use app\core\Application;
+use app\core\Model;
+
+/**
+* The User class would represent the database models for the user.
+* They would be responsible for interacting with the database to read, update, and delete data.
+*/
+
+
+class User extends Model {
+
+    public function tableName($tableName): string {
+        return $tableName;
+    }
+
+    
+    public static function prepare($sql){
+        return Application::$app->db->pdo->prepare($sql);
+    }
+
+    // update user method
+
+
+    // delete user method
+
+
+    //logout method
+
+
+    // is logged in method
+
+
+    // is admin method
+
+}
