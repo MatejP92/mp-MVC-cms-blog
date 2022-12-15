ccfb279f12fa258aadbd810ff6120eb32a5d5008
Matej~ <matej.pal@hotmail.com>
2022-12-15 12:32:44 +0100

    updated to show invalid input errors

--- views/login.php
+++ views/login.php
@@ -1,13 +1,20 @@
-<h2 class="text-center">Login</h2>
+<div class="col">
+    <h2 class="text-center">Login</h2>
         <div class="container">
             <form action="" method="post">
                 <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                    <input type="text" class="form-control" name="username" id="username" placeholder="Username/Email...">
+                    <input type="text" class="form-control <?php echo $model->hasError('username') ? 'is-invalid' : '' ?>" name="username" value="<?php echo $model->username ?>" id="username" placeholder="Username/Email...">
                     <label for="username">Username...</label>
+                    <div class="invalid-feedback">
+                        <?php echo $model->getFirstError("username") ?>
+                    </div>
                 </div>
                 <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                    <input type="password" class="form-control" name="password" id="password" placeholder="Password...">
+                    <input type="password" class="form-control <?php echo $model->hasError('password') ? 'is-invalid' : '' ?>" name="password" id="password" placeholder="Password...">
                     <label for="password">Password...</label>
+                    <div class="invalid-feedback">
+                        <?php echo $model->getFirstError("password") ?>
+                    </div>
                 </div>
                 <div class="mx-auto d-grid">
                     <button type="submit" class="mx-auto btn btn-dark col-2" name="submit" id="submit">Login</button>
--- views/register.php
+++ views/register.php
@@ -3,20 +3,32 @@
     <div class="container">
         <form action="" method="post">
             <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                <input type="text" class="form-control" name="username" id="username" placeholder="Username...">
+                <input type="text" class="form-control <?php echo $model->hasError('username') ? 'is-invalid' : '' ?>" name="username" value="<?php echo $model->username ?>" id="username" placeholder="Username...">
                 <label for="username">Username...</label>
+                <div class="invalid-feedback">
+                    <?php echo $model->getFirstError("username") ?>
+                </div>
             </div>
             <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                <input type="text" class="form-control" name="email" id="email" placeholder="Email...">
+                <input type="text" class="form-control <?php echo $model->hasError('email') ? 'is-invalid' : '' ?>" name="email" value="<?php echo $model->email ?>" id="email" placeholder="Email...">
                 <label for="email">Email...</label>
+                <div class="invalid-feedback">
+                    <?php echo $model->getFirstError("email") ?>
+                </div>
             </div>
             <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                <input type="password" class="form-control" name="password" id="password" placeholder="Password...">
+                <input type="password" class="form-control <?php echo $model->hasError('password') ? 'is-invalid' : '' ?>" name="password" id="password" placeholder="Password...">
                 <label for="password">Password...</label>
+                <div class="invalid-feedback">
+                    <?php echo $model->getFirstError("password") ?>
+                </div>
             </div>
             <div class="mx-auto form-floating mb-3 mt-3 col-4">
-                <input type="password" class="form-control" name="repeatPassword" id="repeatPassword" placeholder="RepeatPassword...">
+                <input type="password" class="form-control <?php echo $model->hasError('password') ? 'is-invalid' : '' ?>" name="repeatPassword" id="repeatPassword" placeholder="RepeatPassword...">
                 <label for="repeatPassword">Repeat Password...</label>
+                <div class="invalid-feedback">
+                    <?php echo $model->getFirstError("repeatPassword") ?>
+                </div>
             </div>
             <div class="mx-auto d-grid">
                 <button type="submit" class="mx-auto btn btn-dark col-2" name="submit" id="submit">Register</button>
