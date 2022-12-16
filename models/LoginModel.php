bf571054673befd4bbb17a43e8dfa6dff6a51733
Matej~ <matej.pal@hotmail.com>
2022-12-16 16:04:47 +0100

    update

--- models/LoginModel.php
+++ models/LoginModel.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace app\models;
+use app\core\Application;
 use app\core\Model;
+use app\models\User;
 
 /**
 * The LoginModel class would represent the database models for the user.
@@ -10,22 +12,32 @@ use app\core\Model;
 
 class LoginModel extends Model {
 
-   public string $username = "";
-   public string $password = "";
+    public string $username = "";
+    public string $password = "";
 
-
-   // login method
+     // login method
+    public function tableName(): string {
+        return "users";
+    }
 
     public function login() {
-      echo "login in";
-   }
+        $user = User::findUser(["username" => $this->username]); // this method finds the user by username
+        if(!$user){ // add an error message to display that the user doesn't exist
+            $this->addError("username", "User with this username doesn't exist");
+            return false;
+        }
+        if(!password_verify($this->password, $user->password)){
+            $this->addError("password", "Incorrect password");
+            return false;
+        }
+        return Application::$app->login($user);
+    }
 
 
-   public function rules(): array {
-      return [
-          "username" => [self::RULE_REQUIRED],
-          "password" => [self::RULE_REQUIRED],
-      ];
-  }
-
+    public function rules(): array {
+        return [
+            "username" => [self::RULE_REQUIRED],
+            "password" => [self::RULE_REQUIRED],
+            ];
+    }
 }
