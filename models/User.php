885ff28be0ba12c12b63757d1326a56272f43a44
Matej~ <matej.pal@hotmail.com>
2022-12-16 16:03:50 +0100

    renamed from RegisterModel

--- models/User.php
+++ models/User.php
@@ -2,25 +2,61 @@
 
 namespace app\models;
 use app\core\Application;
-use app\core\Model;
+use app\core\DatabaseModel;
 
 /**
-* The User class would represent the database models for the user.
-* They would be responsible for interacting with the database to read, update, and delete data.
+* The User class would represent the database models for the user registration.
+* They would be responsible for interacting with the database to create, read, update, and delete data.
 */
 
+class User extends DatabaseModel {
 
-class User extends Model {
+    public string $username = "";
+    public string $email = "";
+    public string $password = "";
+    public string $repeatPassword = "";
 
-    public function tableName($tableName): string {
-        return $tableName;
+    public static function tableName(): string {
+        return "users";
+    }
+
+    public static function primaryKey(): string {
+        return "id";
+    }
+ 
+    // register user method
+    public function save(){
+        $data = [
+            "username" => $this->username,
+            "email" => $this->email,
+            "password" => $this->password,
+            "repeatPassword" => $this->repeatPassword
+        ];
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::save();
+    }
+
+    public function attributes(): array {
+        return [
+            "username",
+            "email",
+            "password",
+        ];
+        
+	}
+
+    public function rules(): array {
+        return [
+            "username" => [self::RULE_REQUIRED, [self::RULE_UNIQUE, "class" => self::class], self::RULE_NO_SPECIAL_CHARS],
+            "email" => [self::RULE_REQUIRED, self::RULE_EMAIL, [self::RULE_UNIQUE, "class" => self::class],],
+            "password" => [self::RULE_REQUIRED, [self::RULE_MIN, "min" => 5]],
+            "repeatPassword" => [self::RULE_REQUIRED, [self::RULE_MATCH, "match" => "password"]]
+        ];
     }
 
     
-    public function prepare($sql){
-        return Application::$app->db->pdo->prepare($sql);
-    }
 
+    
     // update user method
 
 
@@ -36,11 +72,7 @@ class User extends Model {
     // is admin method
 
 
-
-	/**
-	 * @return array
-	 */
-	public function rules(): array {
-		return parent::rules();
+	public function getDisplayName(): string {
+        return $this->username;
 	}
 }
