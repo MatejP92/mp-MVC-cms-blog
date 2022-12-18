32d1e7c6d1c45d907cf5c2537f75f6316b10f043
Matej~ <matej.pal@hotmail.com>
2022-12-18 13:07:00 +0100

    testing

---
+++ views/layouts/shared/admin_navbar.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark justify-content-between">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Home</a>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="#">Profile</a>
+            <a class="nav-item nav-link" href="#">Logout</a>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+<nav class="navbar navbar-dark bg-dark flex-column flex-md-row navbar-expand-md navbar-expand-lg navbar-expand-xl navbar-expand navbar-fixed-side">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Dashboard</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Posts
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">View All Posts</a>
+                        <a class="dropdown-item" href="#">Create Post</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Comments</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Users
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">View All Users</a>
+                        <a class="dropdown-item" href="#">Add User</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+<nav class="nav flex-column">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+</nav>
