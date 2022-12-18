59824ec24489a8321279ba78349babe35797137e
Matej~ <matej.pal@hotmail.com>
2022-12-18 13:03:49 +0100

    added construct and admin methods

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,6 +1,29 @@
 <?php
 
+namespace app\controllers;
+use app\core\Controller;
+use app\core\middlewares\AuthMiddleware;
+
 /**
- *  The AdminController class would handle all of the logic
- *  for the admin dashboard, including creating and editing posts 
- **/
+* class AdminController
+* 
+* @author  Matej Pal <matejpal92@gmail.com>
+* @package app\controllers
+*/
+
+/**
+*  The AdminController class would handle all of the logic
+*  for the admin dashboard, including creating and editing posts 
+**/
+
+class AdminController extends Controller {
+    
+    public function __construct() {
+        $this->registerMiddleware(new AuthMiddleware(["admin"]));
+    }
+
+    public function admin(){
+        $this->setLayout("admin");
+        return $this->render("admin");
+    }
+}
