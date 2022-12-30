5c7d1e83f854edbec8adcfe918779aa4bb15f8ee
Matej~ <matej.pal@hotmail.com>
2022-12-30 17:00:18 +0100

    updated and added posts to view

--- views/home.php
+++ views/home.php
@@ -2,11 +2,45 @@
 
 <h1>Home</h1>
 
-<h3>Welcome <?php echo $name ?>
-
 <?php if(Application::isGuest()): ?>
 
-<h6>If you are not registered, you can <a href="register">REGISTER</a> here.</h6>
-<h6>If you are already registered, you can <a href="login">LOGIN</a> here.</h6> 
+    <h3>Welcome Guest </h3>
 
-<?php endif;?>
+    <h6>If you are not registered, you can <a href="register">REGISTER</a> here.</h6>
+    <h6>If you are already registered, you can <a href="login">LOGIN</a> here.</h6> 
+
+<?php elseif(!Application::isGuest()): ?>
+
+    <h3>Welcome <?php echo Application::$app->user->getDisplayName() ?> </h3>
+    <br>
+    <h4>Your Posts</h4>
+
+    <!-- Display posts from a logged in user -->
+    <hr>
+    <?php if(!empty($posts)): ?>
+        <?php foreach($posts as $post): ?>
+
+            <!-- POST TITLE GOES HERE -->
+        <h3>
+            <a href=""><?php echo $post->title ?></a>
+        </h3>
+        <!-- POST AUTHOR GOES HERE -->
+        <p class="lead">
+            by <a href=""><?php echo $post->author ?></a>
+        </p>
+        <!-- POST CREATION DATE GOES HERE -->
+        <p><span class="glyphicon glyphicon-time"></span><?php echo $post->created ?></p>
+        <hr>
+        <!-- POST CONTENT GOES HERE -->
+        <p><?php echo $post->content ?></p>  
+        <small><a class="btn btn-secondary" href="#">Read More</a></small> <!-- Read More takes us to the full post -->
+        <hr><hr>
+
+        <?php endforeach; ?>
+    
+    <?php else: ?>
+        <p>You have no published posts!</p>
+
+    <?php endif; ?>
+
+<?php endif; ?>
--- views/posts.php
+++ views/posts.php
@@ -3,4 +3,25 @@
 $this->title = " - Posts"; 
 ?>
 
-<h3>POSTS</h3>
+<h2>POSTS</h2>
+<hr>
+
+<?php foreach($posts as $post): ?>
+
+    <!-- POST TITLE GOES HERE -->
+<h3>
+    <a href=""><?php echo $post->title ?></a>
+</h3>
+<!-- POST AUTHOR GOES HERE -->
+<p class="lead">
+    by <a href=""><?php echo $post->author ?></a>
+</p>
+<!-- POST CREATION DATE GOES HERE -->
+<p><?php echo $post->created ?></p>
+<hr>
+<!-- POST CONTENT GOES HERE -->
+<p><?php echo $post->content ?></p>  
+<small><a class="btn btn-secondary" href="#">Read More</a></small> <!-- Read More takes us to the full post -->
+<hr><hr>
+
+<?php endforeach; ?>
