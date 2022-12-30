f70f8bfba542691a4c7246e119fb356af3a51f2b
Matej~ <matej.pal@hotmail.com>
2022-12-30 17:00:44 +0100

    added view posts table

--- views/admin/view_posts.php
+++ views/admin/view_posts.php
@@ -1,1 +1,45 @@
-<h2>View All Posts</h2>
+<br><h2>View All Posts</h2><br>
+
+<!-- <table border="2">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Content</th>
+            <th>Creation Date</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <?php foreach($posts as $post): ?>
+    <tr>
+        <td><?php echo $post->title ?></td>
+        <td><?php echo $post->author ?></td>
+        <td><?php echo $post->content ?></td>
+        <td><?php echo $post->created ?></td>
+        <td><?php echo $post->status ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table> -->
+
+<table class="table table-dark table-hover">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Content</th>
+            <th>Creation Date</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($posts as $post): ?>
+        <tr>
+            <td><?php echo $post->title ?></td>
+            <td><?php echo $post->author ?></td>
+            <td><?php echo $post->content ?></td>
+            <td><?php echo $post->created ?></td>
+            <td><?php echo $post->status ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
